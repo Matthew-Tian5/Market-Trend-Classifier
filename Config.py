54f02d790d63601c2d61a1efbd6473dc8138e169
Matthew-Tian5 <matthew.tian2005@gmail.com>
2026-05-15 18:04:06 -0400

    Stores the database credentials and settings

---
+++ Config.py
@@ -0,0 +1,11 @@
+DB_CONFIG = {
+    "host": "localhost",
+    "port": 5432,
+    "database": "market_db",
+    "user": "youruser",
+    "password": "yourpassword"
+}
+
+TICKERS    = ["AAPL", "MSFT", "NVDA"]
+START_DATE = "2022-01-01"
+END_DATE   = "2024-12-31"
