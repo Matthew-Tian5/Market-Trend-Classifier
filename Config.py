b353896c94a569d9e14bd217e9d939e262493c9b
Matthew-Tian5 <matthew.tian2005@gmail.com>
2026-05-15 19:11:59 -0400

    updated config

--- Config.py
+++ Config.py
@@ -2,10 +2,9 @@ DB_CONFIG = {
     "host": "localhost",
     "port": 5432,
     "database": "market_db",
-    "user": "youruser",
-    "password": "yourpassword"
+    "user": "matthewtian",  # your Mac username change as needed
+    "password": ""
 }
-
 TICKERS    = ["AAPL", "MSFT", "NVDA"]
 START_DATE = "2022-01-01"
 END_DATE   = "2024-12-31"
